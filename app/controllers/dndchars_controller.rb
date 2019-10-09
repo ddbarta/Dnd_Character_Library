866a6a36fd7ff4000f407d77bbb5402d1cb99c57
ddbarta5 <ddbarta5@gmail.com>
2019-10-08 22:55:52 -0400

    Create 'new' view

--- app/controllers/dndchars_controller.rb
+++ app/controllers/dndchars_controller.rb
@@ -4,8 +4,22 @@ class DndcharsController < ApplicationController
 
   end
 
+  def new
+    @dndchar = Dndchar.new
+  end
 
+  def create
+    @dndchar = Dndchar.new(dndchar_params)
 
+    if @dndchar.save
+      redirect_to @dndchar
+    else
+      render 'new'
+    end
+  end
 
-
+  private
+  def dndchar_params
+    params.require(:dndchar).permit(:charname, :charrace, :charclass, :charlevel)
+  end
 end
